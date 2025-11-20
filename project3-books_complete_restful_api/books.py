922d640571d56b1765f6bc6a633a0b0260810762
carlos-dani-dev <carlos.rocha@ufpi.edu.br>
2025-11-20 15:03:13 -0300

    project3 folder renamed

---
+++ project3-books_complete_restful_api/books.py
@@ -0,0 +1,129 @@
+from typing import Optional
+from fastapi import FastAPI, Path, Query, HTTPException, status
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Book:
+    id: str
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+    
+    
+    def __init__(self, id, title, author, description, rating, published_date):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+        
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
+    
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "codingwithroby",
+                "description": "A new description of a book",
+                "rating": 5,
+                "published_date": 2019
+            }
+        }
+    }
+
+
+BOOKS=[
+    Book(1, "Computer Science Pro", "codingwithroby", "A very nice book", 5, 2012),
+    Book(2, "Be Fast with FastAPI", "codingwithroby", "A great book", 5, 2020),
+    Book(3, "Master Endpoints", "codingwithroby", "A awesome book", 5, 2021),
+    Book(4, "HP1", "Author 1", "Book Description", 2, 2014),
+    Book(5, "HP2", "Author 2", "Book Description", 3, 2019),
+    Book(6, "HP3", "Author 3", "Book Description", 1, 2020)
+]
+
+
+@app.get("/books", status_code = status.HTTP_200_OK)
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/{book_id}", status_code = status.HTTP_200_OK)
+async def read_book(book_id: int = Path(gt=0)):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code = 404, detail="Item not found")
+
+
+# get book by published date with dynamic param
+@app.get("/books/published_date/", status_code = status.HTTP_200_OK)
+async def read_book_by_published_date(published_date: int = Query(gt=1999)):
+    books_to_return = []
+    for i in range(len(BOOKS)):
+        if BOOKS[i].published_date == published_date:
+            books_to_return.append(BOOKS[i])
+    return books_to_return
+
+
+@app.get("/books/", status_code = status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/create_book", status_code = status.HTTP_201_CREATED)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    print(type(new_book))
+    BOOKS.append(find_book_id(new_book))
+    
+
+def find_book_id(book: Book):
+        
+    book.id = BOOKS[-1].id + 1 if len(BOOKS) > 0 else 1
+        
+    return  book
+
+
+# update a piece of data
+@app.put("/books/update_book", status_code = status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_change = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            book_change = True
+    if not book_change: raise HTTPException(status_code = 404, detail="Item not found")
+
+
+# delete a piece of data
+@app.delete("/books/{book_id}", status_code = status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_change = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_change = True
+            break
+    if not book_change: raise HTTPException(status_code = 404, detail="Item not found")
+
+
+# Query e Path objects fazem a validação de valores que não são BaseModel do Pydantic
+# Uma vez que os métodos foram bem sucedidos, os valores de status code previsto em cada
+#função são retornados. Se não forem bem sucedidos, ocorre o raise do error
